87138aefddac33011a10e3069a26aa710c5b096f
panda0330 <superdev330@gmail.com>
2023-11-08 22:30:25 +0900

    first

---
+++ server/app/main.py
@@ -0,0 +1,56 @@
+import time
+from pathlib import Path
+
+from fastapi import FastAPI, APIRouter, Request, Depends
+from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+
+from app import crud
+from app.core.dependencies import get_db
+from app.api.api_v1 import api_router
+from app.core.config import settings
+
+
+# Project Directories
+BASE_PATH = Path(__file__).resolve().parent
+TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "templates"))
+
+
+app = FastAPI(title="Recipe API", openapi_url=f"{settings.API_V1_STR}/openapi.json")
+root_router = APIRouter()
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+
+    return response
+
+
+# Updated to serve a Jinja2 template
+# https://www.starlette.io/templates/
+# https://jinja.palletsprojects.com/en/3.0.x/templates/#synopsis
+@root_router.get("/", status_code=200)
+def root(request: Request, db: Session = Depends(get_db)) -> dict:
+    """
+    Root GET
+    """
+    recipes = crud.recipe.get_multi(db=db, limit=10)
+    return TEMPLATES.TemplateResponse(
+        "index.html",
+        {"request": request, "recipes": recipes}
+    )
+
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
+app.include_router(root_router)
+
+
+if __name__ == "__main__":
+    # Use this for debugging purpose only
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8081, log_level="debug")
